333f047e55882827b7afc6db76cbb63f2b28cabe
Zane Peckenpaugh <zanepeck01@gmail.com>
2025-11-04 19:06:22 -0500

    created basic NN

--- introduction_example/src/main/java/com/introduction_example/Main.java
+++ introduction_example/src/main/java/com/introduction_example/Main.java
@@ -1,7 +1,37 @@
 package com.introduction_example;
 
+import ai.djl.*;
+import ai.djl.nn.*;
+import ai.djl.nn.core.*;
+import ai.djl.training.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Application application = Application.CV.IMAGE_CLASSIFICATION;
+
+        long intputSize = 28 * 28;
+        long outputSize = 10;
+
+        SequentialBlock block = new SequentialBlock();
+
+        /*
+         * This is the Seqeuntial Block:
+         * SequentialBlock {
+         * batchFlatten
+         * Linear
+         * LambdaBlock
+         * Linear
+         * LambdaBlock
+         * Linear
+         * }
+         */
+        block.add(Blocks.batchFlattenBlock(intputSize));
+        block.add(Linear.builder().setUnits(128).build());
+        block.add(Activation::relu);
+        block.add(Linear.builder().setUnits(64).build());
+        block.add(Activation::relu);
+        block.add(Linear.builder().setUnits(outputSize).build());
+
     }
 }
